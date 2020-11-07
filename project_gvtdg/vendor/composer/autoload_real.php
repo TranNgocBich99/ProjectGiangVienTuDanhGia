5953868da8428a7d7aa21a1e899c52de1265add3
unknown <tranngocbich_t62@hus.edu.vn>
2020-11-07 10:45:46 +0700

    fe: trang chu sua

--- project_gvtdg/vendor/composer/autoload_real.php
+++ project_gvtdg/vendor/composer/autoload_real.php
@@ -13,11 +13,16 @@ class ComposerAutoloaderInitbd130ba63f8ea5d74e3bbc5bfe681c94
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
+
+        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitbd130ba63f8ea5d74e3bbc5bfe681c94', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -25,7 +30,7 @@ class ComposerAutoloaderInitbd130ba63f8ea5d74e3bbc5bfe681c94
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitbd130ba63f8ea5d74e3bbc5bfe681c94::getInitializer($loader));
         } else {
